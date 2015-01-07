9c1207514e94076a80f0426f17c0d2092c4995ca
ponlok huot <phuot1980@gmail.com>
2015-01-07 14:58:47 -0800

    added show html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,54 @@
 class UsersController < ApplicationController
 	def index
 		@users = User.all
+  end
+
+  def show
+  	#puts params['id']
+  	@user = User.find(params[:id])
+  end
+
+  def destroy
+  	@user = User.find(params[:id])
+  	@user.destroy 
+  	redirect_to users_url
+  end
+
+  def new
+  	@user = User.new	
+  end
+
+  def create
+  	@user = User.new(user_params)
+		if @user.save
+			redirect_to users_path
+		else
+			#redisplay the form that is created. 
+			render 'new'
+		end
+  end
+
+  def edit
+		#this action is responsible for retrieving a specific User from db
+		@user = User.find(params[:id])
+	end
+
+
+	def update
+		#this line of code retrieves a spefic bean from the DB
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			redirect_to users_path
+		else
+			render 'edit'
+		end
+	end
+
+  #only accessible from the controller
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :email, :address, :city, :zip)
 	end
 
 end
