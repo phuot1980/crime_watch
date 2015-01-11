3296ccd5f1028386380282c05818e1220a42411c
ponlok huot <phuot1980@gmail.com>
2015-01-10 23:32:12 -0800

    updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-puts "seeding DB"
+puts "Droping DB"
 User.destroy_all
 Report.destroy_all
 
 
 puts "seeding Users DB"
-u1 = User.new(name: 'Ponlok', email: 'Huot', address: '1234 something ave', city: 'rosemead', zip: '92881').save(validate: false)
-u2 = User.new(name: 'Yess', email: 'Limon', address: '1914 park st', city: 'whittier', zip: '90605').save(validate: false)
+u1 = User.new(name: 'Ponlok Huot', email: 'huot@blah.com', address: '1234 something ave', city: 'rosemead', zip: '92881')
+u1.password = 'password'
+u1.save!
+u2 = User.new(name: 'Yess Limon', email: 'limon@blah.com', address: '1914 park st', city: 'whittier', zip: '90605')
+u2.password = 'password'
+u2.save!
 
 puts 'seeding reports db'
-r1 = Report.new(name: 'robbery', user: u1 ).save(validate: false)
-
-
-
+r1 = Report.new(name: 'robbery', user: u1 ).save!
 
 puts "seeding completed"
