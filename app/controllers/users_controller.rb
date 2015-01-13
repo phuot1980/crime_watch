afddea85bb7e18f98db30bbed0c879432d8ff475
ponlok huot <phuot1980@gmail.com>
2015-01-13 08:12:30 -0800

    all save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
 	end
 
 	def user_params
-		params.require(:user).permit(:name, :email, :address, :city, :zip, :password, :password_confirmation)
+		params.require(:user).permit(:name, :email, :address, :city, :zip, :password, :password_confirmation, :image)
 	end
 
 end
